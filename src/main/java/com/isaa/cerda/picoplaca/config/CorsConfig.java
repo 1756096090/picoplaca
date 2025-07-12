bdff7905ee0adf9db2343a14cbfcade136bbea64
Isaac Cerda <isaaccs2003@gmail.com>
2025-07-12 12:37:01 -0500

    Actualizar configuración CORS para permitir orígenes desde cualquier dominio

--- src/main/java/com/isaa/cerda/picoplaca/config/CorsConfig.java
+++ src/main/java/com/isaa/cerda/picoplaca/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")              // aplica a todas las rutas /api/...
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)             // si necesitas enviar cookies o auth
